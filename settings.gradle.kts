138f9442536e7f4cc85e18c0c8b425fbf7915580
marcio <viana_marcio@outlook.com>
2022-04-09 01:31:39 -0300

    Example of variable types

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "TiposVariaveis"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    //var numero: Double = 3.5
+    //var numero2: Float = 5.3f
+
+    //var numeroInteiro: Int = 10
+    //var numeroLong: Long = 100000000L
+
+    var resultado: Boolean = true
+
+    //var texto: String = "Jamilton é um Professor"
+
+    println(resultado)
+}
